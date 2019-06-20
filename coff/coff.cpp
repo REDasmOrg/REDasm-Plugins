4cfd2a3f437cbcff99e30040a51e07730d162dae
Dax <trogu.davide@gmail.com>
2019-06-20 20:51:17 +0200

    Use Context::disassembler()

--- coff/coff.cpp
+++ coff/coff.cpp
@@ -8,12 +8,13 @@ REDASM_PLUGIN("Dax", "COFF Parser", "MIT", 1)
 
 REDASM_EXEC
 {
-    if(!args.expect({ArgumentType::Pointer, ArgumentType::Pointer, ArgumentType::Integer}))
+    if(!args.expect({ArgumentType::Pointer, ArgumentType::Integer}))
         return false;
 
-    Loader* loader = args[0].pointer<Loader>();
-    u8* data = args[1].pointer<u8>();
-    size_t count = args[2].integer<size_t>();
+    Disassembler* disassembler = r_ctx->disassembler();
+    Loader* loader = disassembler->loader();
+    u8* data = args[0].pointer<u8>();
+    size_t count = args[1].integer<size_t>();
 
     COFFSymbolTable coff(data, count);
 
--- msvcrtti/msvcrtti.cpp
+++ msvcrtti/msvcrtti.cpp
@@ -7,16 +7,15 @@ REDASM_PLUGIN("MSVC RTTI Parser", "Dax", "MIT", 1)
 
 REDASM_EXEC
 {
-    if(!args.expect({ArgumentType::Pointer, ArgumentType::Integer}))
+    if(!args.expect({ArgumentType::Integer}))
         return false;
 
-    Disassembler* disassembler = args[0].pointer<Disassembler>();
-    size_t bits = args[1].integer<size_t>();
+    size_t bits = args[0].integer<size_t>();
 
     if(bits == 64)
-        RTTIMsvc<u64>(disassembler).search();
+        RTTIMsvc<u64>(r_ctx->disassembler()).search();
     else
-        RTTIMsvc<u32>(disassembler).search();
+        RTTIMsvc<u32>(r_ctx->disassembler()).search();
 
     return true;
 }
